43bccbba5d5bb4fdbd29357669dd61a96e830c3f
Gussephe Benjamin <gussephe.ayra@utec.edu.pe>
2024-09-07 19:59:48 -0500

    templatesTarea1Corrección

--- TAREAS/ListasEnlazadas/ForwardList.cpp
+++ TAREAS/ListasEnlazadas/ForwardList.cpp
@@ -1,39 +1,28 @@
-#include "ForwardList.h"
-// Constructor
-template <typename T>
-ForwardList<T>::ForwardList() : head(nullptr) {}
+#include"ForwardList.h"
 
-// Destructor
-template <typename T>
-ForwardList<T>::~ForwardList() {
-    clear();
-}
-
-// Devuelve el primer nodo
 template <typename T>
 Node<T>* ForwardList<T>::front() {
-    return head;
+    Node<T>* temp = head;
+    return temp;
 }
 
-// Devuelve el último nodo
 template <typename T>
 Node<T>* ForwardList<T>::back() {
     Node<T>* temp = head;
-    if (!temp) return nullptr;
-    while (temp->next != nullptr) {
+    while (temp != nullptr) {
         temp = temp->next;
     }
     return temp;
 }
 
-// Agrega un nodo al final
 template <typename T>
 void ForwardList<T>::push_back(T val) {
     Node<T>* nodo_val = new Node<T>(val);
-    if (!head) {
+    Node<T>* temp = head;
+
+    if (temp == nullptr) {
         head = nodo_val;
     } else {
-        Node<T>* temp = head;
         while (temp->next != nullptr) {
             temp = temp->next;
         }
@@ -41,106 +30,160 @@ void ForwardList<T>::push_back(T val) {
     }
 }
 
-// Agrega un nodo al inicio
 template <typename T>
 void ForwardList<T>::push_front(T val) {
     Node<T>* nodo_val = new Node<T>(val);
-    nodo_val->next = head;
-    head = nodo_val;
+    Node<T>* temp = head;
+
+    if (temp == nullptr) {
+        head = nodo_val;
+    } else {
+        nodo_val->next = head;
+        head = nodo_val;
+    }
 }
 
-// Elimina el nodo al inicio
+template <typename T>
+void ForwardList<T>::printList() {
+    Node<T>* temp = head;
+    while (temp != nullptr) {
+        cout << temp->val << " -> ";
+        temp = temp->next;
+    }
+    cout << "nullptr" << endl;
+}
+
+template <typename T>
+ForwardList<T>::ForwardList() : head(nullptr) {
+    // Inicializa head como nullptr
+}
+
+template <typename T>
+ForwardList<T>::~ForwardList() {
+    clear();  // Libera todos los nodos al destruir el objeto
+}
+
 template <typename T>
 void ForwardList<T>::pop_front() {
-    if (!head) return;
     Node<T>* temp = head;
     head = head->next;
     delete temp;
 }
 
-// Elimina el nodo al final
 template <typename T>
 void ForwardList<T>::pop_back() {
-    if (!head) return;
-    if (!head->next) {
-        delete head;
+    Node<T>* temp = head;
+
+    if (temp == nullptr) return; // Lista vacía
+
+    if (temp->next == nullptr) { // Solo un elemento
+        delete temp;
         head = nullptr;
         return;
     }
-    Node<T>* temp = head;
+
     while (temp->next->next != nullptr) {
         temp = temp->next;
     }
+
     delete temp->next;
     temp->next = nullptr;
 }
 
-// Accede al valor por índice
 template <typename T>
 T ForwardList<T>::operator[](int index) {
     Node<T>* temp = head;
-    int count = 0;
-    while (temp && count < index) {
-        temp = temp->next;
-        count++;
+
+    if (index == 0) {
+        return head->val;
+    } else if (index < 0 || head == nullptr) {
+        throw out_of_range("Index out of range");
+    } else {
+        int count = 0;
+        while (count < index && temp != nullptr) {
+            count++;
+            temp = temp->next;
+        }
+        if (temp == nullptr) throw out_of_range("Index out of range");
+        return temp->val;
     }
-    return temp ? temp->val : T(); // Devuelve un valor por defecto si el índice es inválido
 }
 
-// Verifica si la lista está vacía
 template <typename T>
 bool ForwardList<T>::empty() {
     return head == nullptr;
 }
 
-// Devuelve el tamaño de la lista
 template <typename T>
 int ForwardList<T>::size() {
-    int count = 0;
     Node<T>* temp = head;
-    while (temp) {
+    int count = 0;
+    while (temp != nullptr) {
         count++;
         temp = temp->next;
     }
     return count;
 }
 
-// Limpia la lista
 template <typename T>
 void ForwardList<T>::clear() {
-    while (head) {
-        Node<T>* temp = head;
+    while (head != nullptr) {
+        Node<T>* auxiliar = head;
         head = head->next;
-        delete temp;
+        delete auxiliar;
     }
 }
 
-// Ordena la lista usando Merge Sort
+template <typename T>
+Node<T>* getMiddle(Node<T>* head) {
+    if (head == nullptr) return head;
+
+    Node<T>* slow = head;
+    Node<T>* fast = head->next;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
 template <typename T>
 Node<T>* merge(Node<T>* left, Node<T>* right) {
-    if (!left) return right;
-    if (!right) return left;
+    if (left == nullptr) return right;
+    if (right == nullptr) return left;
+
+    Node<T>* result = nullptr;
+
     if (left->val <= right->val) {
-        left->next = merge(left->next, right);
-        return left;
+        result = left;
+        result->next = merge(left->next, right);
     } else {
-        right->next = merge(left, right->next);
-        return right;
+        result = right;
+        result->next = merge(left, right->next);
     }
+
+    return result;
 }
 
 template <typename T>
 Node<T>* mergeSort(Node<T>* head) {
-    if (!head || !head->next) return head;
-    Node<T>* slow = head;
-    Node<T>* fast = head->next;
-    while (fast && fast->next) {
-        slow = slow->next;
-        fast = fast->next->next;
+    if (head == nullptr || head->next == nullptr) {
+        return head;
     }
-    Node<T>* mid = slow->next;
-    slow->next = nullptr;
-    return merge(mergeSort(head), mergeSort(mid));
+
+    Node<T>* middle = getMiddle(head);
+    Node<T>* nextToMiddle = middle->next;
+
+    middle->next = nullptr;
+
+    Node<T>* left = mergeSort(head);
+    Node<T>* right = mergeSort(nextToMiddle);
+
+    Node<T>* sortedList = merge(left, right);
+
+    return sortedList;
 }
 
 template <typename T>
@@ -148,28 +191,18 @@ void ForwardList<T>::sort() {
     head = mergeSort(head);
 }
 
-// Invierte la lista
 template <typename T>
 void ForwardList<T>::reverse() {
-    Node<T>* prev = nullptr;
-    Node<T>* current = head;
-    Node<T>* next = nullptr;
-    while (current) {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
-    }
-    head = prev;
-}
-
-// Imprime la lista
-template <typename T>
-void ForwardList<T>::printList() {
     Node<T>* temp = head;
-    while (temp) {
-        cout << temp->val << " -> ";
+    Node<T>* retro = nullptr;
+    Node<T>* prev = nullptr;
+
+    while (temp != nullptr) {
+        prev = retro;
+        retro = temp;
         temp = temp->next;
+        retro->next = prev;
     }
-    cout << "nullptr" << endl;
+
+    head = retro;
 }
